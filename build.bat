d7f8b4bd0938e4673527b86ce3cf9a1a52ed8df9
Rido <rmpablos@hotmail.com>
2015-09-03 16:45:57 +0200

    Updating Nuget Scripts

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\VsMSBuildCmd.bat"
-msbuild /p:Configuration=Release waslibs.sln
+msbuild /p:Configuration=Release %1
--- createNugetPackages.bat
+++ createNugetPackages.bat
@@ -4,10 +4,17 @@ IF  "%1"=="" (
 	SET _VERSION_=%1
 )
 
-REM call build.bat
+IF EXIST _TempNugets rd /q /s _TempNugets
 
-IF NOT EXIST _TempNugets md _TempNugets
+md _TempNugets
+
+call build.bat waslibs.sln
+
 .nuget\nuget.exe pack src\AppStudio.DataProviders\AppStudio.DataProviders.nuspec -OutPutDirectory _TempNugets -Version %_VERSION_% -Prop Configuration=Release
 .nuget\nuget.exe pack src\AppStudio.Common\AppStudio.Common.nuspec -OutPutDirectory _TempNugets -Version %_VERSION_% -Prop Configuration=Release
+
+.nuget\nuget update waslibs.controls.sln -RepositoryPath _TempNugets
+
+call build.bat waslibs.controls.sln
 .nuget\nuget.exe pack src\AppStudio.Controls\AppStudio.Controls.nuspec -OutPutDirectory _TempNugets -Version %_VERSION_%  -Prop Configuration=Release 
 
