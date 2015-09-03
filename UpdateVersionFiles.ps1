a2385497a3ba67282f92054851488d46f76fed1c
Rido <rmpablos@hotmail.com>
2015-09-03 18:20:35 +0200

    nuget package scripts

---
+++ UpdateVersionFiles.ps1
@@ -0,0 +1,76 @@
+# Enable -Verbose option
+[CmdletBinding()] 
+Param(
+	[Parameter(Mandatory=$True,Position=1)]
+	[string]$NewVersion,
+	[Parameter(Mandatory=$False,Position=2)]
+	[string]$Semantic = ""
+
+)
+
+# Regular expression pattern to find the version in the build number 
+# and then apply it to the assemblies
+$VersionRegex = "\d+\.\d+\.\d+\.\d+"
+$InformationalVersionRegex = "\d+\.\d+\.\d+\-(\w+)"
+
+
+$ScriptPath = $null
+try
+{
+    $ScriptPath = (Get-Variable MyInvocation).Value.MyCommand.Path
+    $ScriptDir = Split-Path -Parent $ScriptPath
+}
+catch {}
+
+if (!$ScriptPath)
+{
+    Write-Error "Current path not found!"
+	exit 1
+}
+
+if($Semantic -eq ""){
+	$NewInformationalVersion = $NewVersion
+}
+else{
+	$NewInformationalVersion = $NewVersion  + "-" + $Semantic
+}
+
+$NewVersion=$NewVersion + ".0"
+
+Write-Host "Version: $NewVersion"
+Write-Host "Informational Version: $NewInformationalVersion"
+Write-Host "ScriptDir: " $ScriptDir
+
+# Apply the version to the assembly property files
+$files = gci $ScriptDir -recurse -include "*Properties*","My Project" | 
+    ?{ $_.PSIsContainer } | 
+    foreach { gci -Path $_.FullName -Recurse -include AssemblyInfo.* }
+
+if($files)
+{
+    Write-Host "Will apply $NewVersion to $($files.count) files."
+
+    foreach ($file in $files) {
+        $filecontent = Get-Content($file)
+        attrib $file -r
+        $filecontent -replace $VersionRegex, $NewVersion | %{ $_ -replace $InformationalVersionRegex, $NewInformationalVersion } | Out-File $file
+
+        Write-Host "$file.FullName - version applied"
+    }
+}
+else
+{
+    Write-Warning "Found no files."
+}
+
+
+$projectJsonRegex = "WindowsAppStudio\.Common.*\d+\.\d+\.\d+\-(\w+)"
+$projectJsonFile = Join-Path $ScriptDir "src\AppStudio.Controls\project.json"
+$newReference = 'WindowsAppStudio.Common": "' + $NewInformationalVersion
+$jsonContent = Get-Content($projectJsonFile)
+attrib $projectJsonFile -r
+$jsonContent -replace $projectJsonRegex, $newReference| Out-File $projectJsonFile
+
+
+
+
--- createNugetPackages.bat
+++ createNugetPackages.bat
@@ -4,17 +4,24 @@ IF  "%1"=="" (
 	SET _VERSION_=%1
 )
 
+IF  "%2"=="" (	
+	SET _FULLVERSION_=%_VERSION_%
+) ELSE (
+	SET _SEMANTIC_=%2
+	SET _FULLVERSION_=%_VERSION_%-%_SEMANTIC_%
+)
+
 IF EXIST _TempNugets rd /q /s _TempNugets
 
 md _TempNugets
 
+powershell.exe -ExecutionPolicy Bypass -Command .\UpdateVersionFiles.ps1 %_VERSION_% %_SEMANTIC_%
+
 call build.bat waslibs.sln
 
-.nuget\nuget.exe pack src\AppStudio.DataProviders\AppStudio.DataProviders.nuspec -OutPutDirectory _TempNugets -Version %_VERSION_% -Prop Configuration=Release
-.nuget\nuget.exe pack src\AppStudio.Common\AppStudio.Common.nuspec -OutPutDirectory _TempNugets -Version %_VERSION_% -Prop Configuration=Release
+.nuget\nuget.exe pack src\AppStudio.DataProviders\AppStudio.DataProviders.nuspec -OutPutDirectory _TempNugets -Version %_FULLVERSION_% -Prop Configuration=Release
+.nuget\nuget.exe pack src\AppStudio.Common\AppStudio.Common.nuspec -OutPutDirectory _TempNugets -Version %_FULLVERSION_% -Prop Configuration=Release
 
-.nuget\nuget update waslibs.controls.sln -RepositoryPath _TempNugets
-
-call build.bat waslibs.controls.sln
-.nuget\nuget.exe pack src\AppStudio.Controls\AppStudio.Controls.nuspec -OutPutDirectory _TempNugets -Version %_VERSION_%  -Prop Configuration=Release 
+call build.bat waslibs.controls.sln /p:NuGetPackagesDirectory=_TempNugets
+.nuget\nuget.exe pack src\AppStudio.Controls\AppStudio.Controls.nuspec -OutPutDirectory _TempNugets -Version %_FULLVERSION_%  -Prop Configuration=Release 
 
