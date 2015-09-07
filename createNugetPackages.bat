1832faa23379bbaafeac67e4e9eb58aa0f69cc0f
Rido <rmpablos@hotmail.com>
2015-09-07 17:26:14 +0200

    update build script

--- createNugetPackages.bat
+++ createNugetPackages.bat
@@ -11,17 +11,18 @@ IF  "%2"=="" (
 	SET _FULLVERSION_=%_VERSION_%-%_SEMANTIC_%
 )
 
-IF EXIST _TempNugets rd /q /s _TempNugets
+IF EXIST _TempNugets del /q /s _TempNugets\WindowsAppStudio*
 
-md _TempNugets
+IF NOT EXIST _TempNugets md _TempNugets
 
-powershell.exe -ExecutionPolicy Bypass -Command .\UpdateVersionFiles.ps1 %_VERSION_% %_SEMANTIC_%
+REM powershell.exe -ExecutionPolicy Bypass -Command .\UpdateVersionFiles.ps1 %_VERSION_% %_SEMANTIC_%
 
 call build.bat waslibs.sln
 
 .nuget\nuget.exe pack src\AppStudio.DataProviders\AppStudio.DataProviders.nuspec -OutPutDirectory _TempNugets -Version %_FULLVERSION_% -Prop Configuration=Release
 .nuget\nuget.exe pack src\AppStudio.Common\AppStudio.Common.nuspec -OutPutDirectory _TempNugets -Version %_FULLVERSION_% -Prop Configuration=Release
 
-call build.bat waslibs.controls.sln /p:NuGetPackagesDirectory=_TempNugets
+.nuget\nuget.exe restore -Source %cd%\_TempNugets waslibs.Controls.sln
+build waslibs.controls.sln
 .nuget\nuget.exe pack src\AppStudio.Controls\AppStudio.Controls.nuspec -OutPutDirectory _TempNugets -Version %_FULLVERSION_%  -Prop Configuration=Release 
 
