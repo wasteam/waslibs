781c9a66347a4dbbe402d9e4e86156e9a8abb553
Martin Vega <mvegaca@outlook.com>
2015-09-11 12:24:51 +0200

    Fix Sample and Add TaskExtension to Uwp

---
+++ src/AppStudio.Uwp/TasksExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppStudio.Uwp
+{
+    public static class TasksExtensions
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "task")]
+        public static void RunAndForget(this Task task)
+        {
+        }
+    }
+}
