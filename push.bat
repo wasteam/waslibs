aaf3717aca7161c83ce5ed97ac2565c4660e1fd6
ralarcon <ralarcon@microsoft.com>
2015-09-15 13:11:05 +0200

    Push not tag version

--- push.bat
+++ push.bat
@@ -8,10 +8,6 @@ IF  "%1"=="" (
 .nuget\nuget.exe push _TempNugets\WindowsAppStudio.Common.%_FULLVERSION_%.nupkg %2 -Source %3
 .nuget\nuget.exe push _TempNugets\WindowsAppStudio.Uwp.%_FULLVERSION_%.nupkg %2 -Source %3
 
-git remote set-url origin https://ralarcon:0cbc2dc736f2af07d324eb9f47f4e81d13df3197@github.com/wasteam/waslibs.git
-git tag v%_FULLVERSION_%
-git push --tag
-
 
 GOTO END
 
