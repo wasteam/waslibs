1fd2a2f1d2cb5a017f0acc9a64a560dbef4c105f
ralarcon <ralarcon@microsoft.com>
2015-09-15 16:52:03 +0200

    Add user identity to git in tagging

--- versiontag.bat
+++ versiontag.bat
@@ -9,7 +9,9 @@ IF  NOT "%2"=="" (
 	SET _TAG_=%_TAG_%.%2
 )
 
-rem git remote set-url origin https://%3:%4@github.com/wasteam/waslibs.git
+git config user.email "%3@outlook.com"
+git config user.name "%3"
+git remote set-url origin https://%3:%4@github.com/wasteam/waslibs.git
 git tag -a %_TAG_% -m "Version built: %_TAG_%"
 git push origin %_TAG_%
 
