6884888e310b91c9ec3af347b9e717ea6e2f2fb8
Javier Sanchez <javier.sanchez.m@live.com>
2015-09-15 11:56:50 +0200

    Update Facebook Graph Api to v2.4

--- src/AppStudio.DataProviders/Facebook/FacebookDataProvider.cs
+++ src/AppStudio.DataProviders/Facebook/FacebookDataProvider.cs
@@ -9,7 +9,7 @@ namespace AppStudio.DataProviders.Facebook
 {
     public class FacebookDataProvider : DataProviderBase<FacebookDataConfig, FacebookSchema>
     {
-        private const string BaseUrl = @"https://graph.facebook.com/v2.2";
+        private const string BaseUrl = @"https://graph.facebook.com/v2.4";
 
         private FacebookOAuthTokens _tokens;
 
@@ -29,7 +29,7 @@ namespace AppStudio.DataProviders.Facebook
 
             var settings = new HttpRequestSettings
             {
-                RequestedUri = new Uri(string.Format("{0}/{1}/posts?&access_token={2}|{3}", BaseUrl, config.UserId, _tokens.AppId, _tokens.AppSecret), UriKind.Absolute)
+                RequestedUri = new Uri(string.Format("{0}/{1}/posts?&access_token={2}|{3}&fields=id,message,from,created_time,link,full_picture", BaseUrl, config.UserId, _tokens.AppId, _tokens.AppSecret), UriKind.Absolute)
             };
 
             HttpRequestResult result = await HttpRequest.DownloadAsync(settings);
