0f5a1a2864b2f3e95e9f710d742514dcc9889b6e
Alvaro Mateos <alvaroma@live.com>
2015-11-26 18:45:49 +0100

    Revert "HtmlViewer + VirtualBox"
    This reverts commit 5fbe1c43d981cacc6913c110e78070881ba57392.

--- samples/AppStudio.Samples.Uwp/App.xaml.cs
+++ samples/AppStudio.Samples.Uwp/App.xaml.cs
@@ -97,9 +97,7 @@ namespace AppStudio.Samples.Uwp
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-
                 rootFrame.Navigate(typeof(MainPage), e.Arguments);
-
             }
             // Ensure the current window is active
             Window.Current.Activate();
