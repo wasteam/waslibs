3a3b0415b7945ff79a396299ba47c32ac58862f8
Julia Piedrahita Alarcón <julia.piedrahita.alarcon@hotmail.es>
2016-03-02 18:19:59 +0100

    YouTube refactoring

---
+++ samples/AppStudio.Uwp.Samples/Common/Providers/RawParser.cs
@@ -0,0 +1,14 @@
+﻿using AppStudio.DataProviders;
+using System.Collections.Generic;
+
+namespace AppStudio.Uwp.Samples
+{
+    class RawParser : IParser<RawSchema>
+    {
+        public IEnumerable<RawSchema> Parse(string data)
+        {
+            var result = new RawSchema[] { new RawSchema { _id = "", Raw = data } };
+            return result;
+        }
+    }
+}
--- samples/AppStudio.Uwp.Samples/Pages/YouTube/YouTubePage.xaml.cs
+++ samples/AppStudio.Uwp.Samples/Pages/YouTube/YouTubePage.xaml.cs
@@ -1,10 +1,8 @@
 ﻿using AppStudio.DataProviders.YouTube;
 using AppStudio.Uwp.Commands;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Net.Http;
 using System.Windows.Input;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Navigation;
@@ -18,6 +16,8 @@ namespace AppStudio.Uwp.Samples
         private const string DefaultYouTubeQueryParam = @"MicrosoftLumia";
         private const YouTubeQueryType DefaultQueryType = YouTubeQueryType.Channels;
         private const int DefaultMaxRecordsParam = 20;
+
+        YouTubeDataProvider _youTubeDataProvider;
         public YouTubePage()
         {
             this.InitializeComponent();
@@ -29,7 +29,7 @@ namespace AppStudio.Uwp.Samples
             get { return "YouTube Data Provider"; }
         }
 
-        #region ApiKey
+        #region DataProvider Config
         public string ApiKey
         {
             get { return (string)GetValue(ApiKeyProperty); }
@@ -38,9 +38,7 @@ namespace AppStudio.Uwp.Samples
 
         public static readonly DependencyProperty ApiKeyProperty = DependencyProperty.Register("ApiKey", typeof(string), typeof(YouTubePage), new PropertyMetadata(DefaultApiKey));
 
-        #endregion
 
-        #region Query
         public string YouTubeQueryParam
         {
             get { return (string)GetValue(YouTubeQueryParamProperty); }
@@ -49,10 +47,7 @@ namespace AppStudio.Uwp.Samples
 
         public static readonly DependencyProperty YouTubeQueryParamProperty = DependencyProperty.Register("YouTubeQueryParam", typeof(string), typeof(YouTubePage), new PropertyMetadata(DefaultYouTubeQueryParam));
 
-        #endregion
 
-        #region QueryType
-      
         public YouTubeQueryType YouTubeQueryTypeSelectedItem
         {
             get { return (YouTubeQueryType)GetValue(YouTubeQueryTypeSelectedItemProperty); }
@@ -61,9 +56,7 @@ namespace AppStudio.Uwp.Samples
 
         public static readonly DependencyProperty YouTubeQueryTypeSelectedItemProperty = DependencyProperty.Register("YouTubeQueryTypeSelectedItem", typeof(YouTubeQueryType), typeof(YouTubePage), new PropertyMetadata(DefaultQueryType));
 
-        #endregion
 
-        #region MaxRecords
         public int MaxRecordsParam
         {
             get { return (int)GetValue(MaxRecordsParamProperty); }
@@ -96,19 +89,19 @@ namespace AppStudio.Uwp.Samples
 
         #endregion
 
-        #region JsonResult
-        public string DataProviderJsonResult
+        #region RawData
+        public string DataProviderRawData
         {
-            get { return (string)GetValue(DataProviderJsonResultProperty); }
-            set { SetValue(DataProviderJsonResultProperty, value); }
+            get { return (string)GetValue(DataProviderRawDataProperty); }
+            set { SetValue(DataProviderRawDataProperty, value); }
         }
 
-        public static readonly DependencyProperty DataProviderJsonResultProperty = DependencyProperty.Register("DataProviderJsonResult", typeof(string), typeof(YouTubePage), new PropertyMetadata(string.Empty));
+        public static readonly DependencyProperty DataProviderRawDataProperty = DependencyProperty.Register("DataProviderRawData", typeof(string), typeof(YouTubePage), new PropertyMetadata(string.Empty));
 
         #endregion    
 
         #region ICommands
-        public ICommand RefreshData
+        public ICommand RefreshDataCommand
         {
             get
             {
@@ -119,24 +112,13 @@ namespace AppStudio.Uwp.Samples
             }
         }
 
-        public ICommand SetDefault
+        public ICommand RestoreConfigCommand
         {
             get
             {
                 return new RelayCommand(() =>
                 {
-                    SetDefaultData();
-                });
-            }
-        }
-
-        public ICommand OpenUrl
-        {
-            get
-            {
-                return new RelayCommand(async () =>
-                {
-                    await Windows.System.Launcher.LaunchUriAsync(new Uri(YouTubeQuery));
+                    RestoreConfig();
                 });
             }
         }
@@ -144,10 +126,10 @@ namespace AppStudio.Uwp.Samples
         #endregion
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
-        {           
+        {
             this.Items = new ObservableCollection<object>();
-            SetDefaultData();
-            Request();         
+            RestoreConfig();
+            Request();
 
             base.OnNavigatedTo(e);
         }
@@ -162,10 +144,10 @@ namespace AppStudio.Uwp.Samples
             try
             {
                 DataProviderError = string.Empty;
-                DataProviderJsonResult = string.Empty;
+                DataProviderRawData = string.Empty;
                 Items.Clear();
 
-                var _youTubeDataProvider = new YouTubeDataProvider(new YouTubeOAuthTokens { ApiKey = ApiKey });
+                _youTubeDataProvider = new YouTubeDataProvider(new YouTubeOAuthTokens { ApiKey = ApiKey });
                 var config = new YouTubeDataConfig
                 {
                     Query = YouTubeQueryParam,
@@ -178,8 +160,10 @@ namespace AppStudio.Uwp.Samples
                     Items.Add(item);
                 }
 
-                HttpClient client = new HttpClient();
-                DataProviderJsonResult = await client.GetStringAsync(new Uri(YouTubeQuery, UriKind.Absolute));
+                var rawParser = new RawParser();
+                var rawData = await _youTubeDataProvider.LoadDataAsync(config, MaxRecordsParam, rawParser);
+                DataProviderRawData = rawData.FirstOrDefault()?.Raw?.ToString();
+
             }
             catch (Exception ex)
             {
@@ -188,34 +172,12 @@ namespace AppStudio.Uwp.Samples
             }
         }
 
-        private void SetDefaultData()
+        private void RestoreConfig()
         {
             ApiKey = DefaultApiKey;
             YouTubeQueryParam = DefaultYouTubeQueryParam;
             YouTubeQueryTypeSelectedItem = DefaultQueryType;
-            MaxRecordsParam = DefaultMaxRecordsParam;           
-        } 
-
-        private string YouTubeQuery
-        {
-            get
-            {
-                switch (YouTubeQueryTypeSelectedItem)
-                {
-                    case YouTubeQueryType.Channels:
-                        return string.Format(@"https://www.googleapis.com/youtube/v3/channels?forUsername={0}&part=contentDetails&maxResults={1}&key={2}", YouTubeQueryParam, MaxRecordsParam, ApiKey);
-
-                    case YouTubeQueryType.Playlist:
-                        return string.Format(@"https://www.googleapis.com/youtube/v3/playlistItems?playlistId={0}&part=snippet&maxResults={1}&key={2}", YouTubeQueryParam, MaxRecordsParam, ApiKey);
-
-                    case YouTubeQueryType.Videos:
-                        return string.Format(@"https://www.googleapis.com/youtube/v3/search?q={0}&part=snippet&maxResults={1}&key={2}&type=video", YouTubeQueryParam, MaxRecordsParam, ApiKey);
-
-                    default:
-                        return string.Format(@"https://www.googleapis.com/youtube/v3/search?q={0}&part=snippet&maxResults={1}&key={2}&type=video", YouTubeQueryParam, MaxRecordsParam, ApiKey);
-                }
-            }
+            MaxRecordsParam = DefaultMaxRecordsParam;
         }
-
     }
 }
