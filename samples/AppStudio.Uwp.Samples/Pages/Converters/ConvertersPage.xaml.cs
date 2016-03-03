f35dfeffda4f42eb5f71957bb6208be54d2dcc57
Martin Vega <mvegaca@outlook.com>
2016-03-03 12:40:01 +0100

    Add Converters to UWP Revisited Sample

---
+++ samples/AppStudio.Uwp.Samples/Pages/Converters/ConvertersPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Navigation;
+using AppStudio.Uwp.Samples.Extensions;
+
+namespace AppStudio.Uwp.Samples
+{
+    [SamplePage(Category = "Tools", Name = "Converters")]
+    public sealed partial class ConvertersPage : SamplePage
+    {
+        public ConvertersPage()
+        {
+            StringVisibilityValue = this.GetResourceString("ConvertersStringVisibilityConverterDefault");
+            this.InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public override string Caption
+        {
+            get { return "Converters"; }
+        }
+
+        #region IsImageVisible        
+        public bool IsImageVisible
+        {
+            get { return (bool)GetValue(IsImageVisibleProperty); }
+            set { SetValue(IsImageVisibleProperty, value); }
+        }
+        public static readonly DependencyProperty IsImageVisibleProperty = DependencyProperty.Register("IsImageVisible", typeof(bool), typeof(ConvertersPage), new PropertyMetadata(true));
+        #endregion
+
+        #region StringVisibilityValue        
+        public string StringVisibilityValue
+        {
+            get { return (string)GetValue(StringVisibilityValueProperty); }
+            set { SetValue(StringVisibilityValueProperty, value); }
+        }
+        public static readonly DependencyProperty StringVisibilityValueProperty = DependencyProperty.Register("StringVisibilityValue", typeof(string), typeof(ConvertersPage), new PropertyMetadata(string.Empty));
+        #endregion
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+        }
+    }
+}
