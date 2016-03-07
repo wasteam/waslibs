33cf616c5154b2d3a760a419684fdb3e7cedc128
Julia Piedrahita Alarcón <julia.piedrahita.alarcon@hotmail.es>
2016-03-07 12:48:22 +0100

    Add LocalStorage DataProvider

---
+++ samples/AppStudio.Uwp.Samples/Pages/LocalStorage/Docs/LocalStorageCSharp.cs
@@ -0,0 +1,51 @@
+﻿using AppStudio.DataProviders.LocalStorage;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace AppStudio.Uwp.Samples
+{
+    public sealed partial class LocalStorageSample : Page
+    {
+        public LocalStorageSample()
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public ObservableCollection<object> Items
+        {
+            get { return (ObservableCollection<object>)GetValue(ItemsProperty); }
+            set { SetValue(ItemsProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemsProperty = DependencyProperty
+            .Register("Items", typeof(ObservableCollection<object>), typeof(LocalStorageSample), new PropertyMetadata(null));
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {           
+            GetItems();
+        }
+
+        public async void GetItems()
+        {
+            string localStorageQuery = "/Assets/LocalStorageSamples.json";
+            int maxRecordsParam = 10;
+
+            Items.Clear();
+            var localStorageDataProvider = new LocalStorageDataProvider<LocalStorageDataSchema>();         
+            var config = new LocalStorageDataConfig { FilePath = localStorageQuery };       
+
+            var items = await localStorageDataProvider.LoadDataAsync(config, maxRecordsParam);           
+
+            foreach (var item in items)
+            {
+                Items.Add(item);
+            }
+        }
+    }
+}
---
+++ samples/AppStudio.Uwp.Samples/Pages/LocalStorage/LocalStorageDataSchema.cs
@@ -0,0 +1,22 @@
+﻿using AppStudio.DataProviders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppStudio.Uwp.Samples
+{
+    public class LocalStorageDataSchema : SchemaBase
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+
+        public string ImageUrl { get; set; }
+
+
+
+    }
+}
