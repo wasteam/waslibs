dda9624dfe9befc3216608c28effd2f357ab3cd6
Alvaro Mateos <alvaroma@live.com>
2016-03-11 04:08:54 +0100

    Added RelativeAndVirtualBox Sample.

---
+++ samples/AppStudio.Uwp.Samples/Pages/RelativeAndVirtualBox/RelativeAndVirtualBoxPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Popups;
+
+namespace AppStudio.Uwp.Samples
+{
+    [SamplePage(Category = "FoundationControls", Name = "RelativeAndVirtualBox", Order = 34)]
+    public sealed partial class RelativeAndVirtualBoxPage : SamplePage
+    {
+        public RelativeAndVirtualBoxPage()
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+            this.Text = "Etiam ac diam nisi, et mattis diam. Praesent congue porta libero a feugiat. Suspendisse in nisl et nisl gravida faucibus. Quisque sagittis sodales faucibus. Integer rhoncus interdum mi, a gravida orci eleifend id. Curabitur tristique, lacus sed volutpat imperdiet, nisl velit pretium sem, et scelerisque quam nibh at libero. Donec felis enim, ornare at molestie ac, fringilla quis dolor. Integer nec orci vel purus sodales aliquet. Quisque ac erat mi. Maecenas in ligula tellus. Quisque non sapien vel diam fringilla lacinia. In et metus odio, vel rhoncus ipsum.";
+        }
+
+        public override string Caption
+        {
+            get { return "RelativeAndVirtualBox Sample"; }
+        }
+
+        #region Text
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(RelativeAndVirtualBoxPage), new PropertyMetadata(null));
+        #endregion
+
+        protected override void OnSettings()
+        {
+            AppShell.Current.Shell.ShowRightPane(new RelativeAndVirtualBoxSettings() { DataContext = control });
+        }
+    }
+}
