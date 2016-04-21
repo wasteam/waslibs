ea07cac0c430fb47d8d334063b427630a0cb5893
Alvaro Mateos <alvaroma@live.com>
2016-04-21 10:14:45 +0200

    Added ResponsiveGridViewEx.

---
+++ src/AppStudio.Uwp/Controls/ResponsiveGridViewEx/ResponsiveGridViewEx.cs
@@ -0,0 +1,151 @@
+﻿using System.Windows.Input;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Controls;
+using Windows.Foundation.Metadata;
+
+namespace AppStudio.Uwp.Controls
+{
+    public sealed class ResponsiveGridViewEx : ListViewBase
+    {
+        private ResponsiveGridViewPanelEx _panel = null;
+
+        public ResponsiveGridViewEx()
+        {
+            this.DefaultStyleKey = typeof(ResponsiveGridViewEx);
+            this.LayoutUpdated += OnLayoutUpdated;
+            this.IsItemClickEnabled = true;
+            this.ItemClick += OnItemClick;
+        }
+
+        #region DesiredWidth
+        public double DesiredWidth
+        {
+            get { return (double)GetValue(DesiredWidthProperty); }
+            set { SetValue(DesiredWidthProperty, value); }
+        }
+
+        public static readonly DependencyProperty DesiredWidthProperty = DependencyProperty.Register("DesiredWidth", typeof(double), typeof(ResponsiveGridViewEx), new PropertyMetadata(0.0));
+        #endregion
+
+        #region DesiredHeight
+        public double DesiredHeight
+        {
+            get { return (double)GetValue(DesiredHeightProperty); }
+            set { SetValue(DesiredHeightProperty, value); }
+        }
+
+        public static readonly DependencyProperty DesiredHeightProperty = DependencyProperty.Register("DesiredHeight", typeof(double), typeof(ResponsiveGridViewEx), new PropertyMetadata(0.0));
+        #endregion
+
+        #region ItemMargin
+        public Thickness ItemMargin
+        {
+            get { return (Thickness)GetValue(ItemMarginProperty); }
+            set { SetValue(ItemMarginProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemMarginProperty = DependencyProperty.Register("ItemMargin", typeof(Thickness), typeof(ResponsiveGridViewEx), new PropertyMetadata(new Thickness(2)));
+        #endregion
+
+        #region ItemPadding
+        public Thickness ItemPadding
+        {
+            get { return (Thickness)GetValue(ItemPaddingProperty); }
+            set { SetValue(ItemPaddingProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemPaddingProperty = DependencyProperty.Register("ItemPadding", typeof(Thickness), typeof(ResponsiveGridViewEx), new PropertyMetadata(new Thickness(2)));
+        #endregion
+
+        #region OneRowModeEnabled
+        public bool OneRowModeEnabled
+        {
+            get { return (bool)GetValue(OneRowModeEnabledProperty); }
+            set { SetValue(OneRowModeEnabledProperty, value); }
+        }
+
+        public static readonly DependencyProperty OneRowModeEnabledProperty = DependencyProperty.Register("OneRowModeEnabled", typeof(bool), typeof(ResponsiveGridViewEx), new PropertyMetadata(false));
+        #endregion
+
+        #region ItemClickCommand
+        public ICommand ItemClickCommand
+        {
+            get { return (ICommand)GetValue(ItemClickCommandProperty); }
+            set { SetValue(ItemClickCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemClickCommandProperty = DependencyProperty.Register("ItemClickCommand", typeof(ICommand), typeof(ResponsiveGridViewEx), new PropertyMetadata(null));
+        #endregion
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new ListViewItem();
+        }
+
+        protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+        {
+            var container = element as ListViewItem;
+            container.Margin = this.ItemMargin;
+            container.Padding = this.ItemPadding;
+            base.PrepareContainerForItemOverride(element, item);
+        }
+
+        private void OnLayoutUpdated(object sender, object e)
+        {
+            if (_panel == null)
+            {
+                _panel = base.ItemsPanelRoot as ResponsiveGridViewPanelEx;
+                if (_panel != null)
+                {
+                    _panel.IsReady = true;
+                    _panel.SetBinding(ResponsiveGridViewPanelEx.DesiredItemWidthProperty, new Binding { Source = this, Path = new PropertyPath("DesiredWidth") });
+                    _panel.SetBinding(ResponsiveGridViewPanelEx.DesiredItemHeightProperty, new Binding { Source = this, Path = new PropertyPath("DesiredHeight") });
+                    _panel.SetBinding(ResponsiveGridViewPanelEx.OneRowModeEnabledProperty, new Binding { Source = this, Path = new PropertyPath("OneRowModeEnabled") });
+                    _panel.InvalidateMeasure();
+                }
+            }
+        }
+
+        private void OnItemClick(object sender, ItemClickEventArgs e)
+        {
+            if (ItemClickCommand != null)
+            {
+                if (ItemClickCommand.CanExecute(e.ClickedItem))
+                {
+                    ItemClickCommand.Execute(e.ClickedItem);
+                }
+            }
+        }
+
+        // Obsolete
+        #region ItemWidth
+        [Deprecated("ItemWidth property will be removed in future versions.", DeprecationType.Deprecate, 65536, "Windows.Foundation.UniversalApiContract")]
+        public double ItemWidth
+        {
+            get { return (double)GetValue(ItemWidthProperty); }
+            set { SetValue(ItemWidthProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemWidthProperty = DependencyProperty.Register("ItemWidth", typeof(double), typeof(ResponsiveGridViewEx), new PropertyMetadata(0.0));
+        #endregion
+
+        #region ItemHeight
+        [Deprecated("ItemHeight property will be removed in future versions.", DeprecationType.Deprecate, 65536, "Windows.Foundation.UniversalApiContract")]
+        public double ItemHeight
+        {
+            get { return (double)GetValue(ItemHeightProperty); }
+            set { SetValue(ItemHeightProperty, value); }
+        }
+
+        private static void ItemHeightChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as ResponsiveGridViewEx;
+            control.DesiredHeight = (double)e.NewValue;
+        }
+
+        public static readonly DependencyProperty ItemHeightProperty = DependencyProperty.Register("ItemHeight", typeof(double), typeof(ResponsiveGridViewEx), new PropertyMetadata(0.0, ItemHeightChanged));
+        #endregion
+    }
+}
---
+++ src/AppStudio.Uwp/Controls/ResponsiveGridViewEx/ResponsiveGridViewPanelEx.cs
@@ -0,0 +1,184 @@
+﻿using System;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.Foundation;
+
+namespace AppStudio.Uwp.Controls
+{
+    public sealed class ResponsiveGridViewPanelEx : Panel
+    {
+        internal bool IsReady { get; set; } = false;
+
+        #region DesiredItemWidth
+        public double DesiredItemWidth
+        {
+            get { return (double)GetValue(DesiredItemWidthProperty); }
+            set { SetValue(DesiredItemWidthProperty, value); }
+        }
+
+        public static readonly DependencyProperty DesiredItemWidthProperty = DependencyProperty.Register("DesiredItemWidth", typeof(double), typeof(ResponsiveGridViewPanelEx), new PropertyMetadata(0.0, DesiredSizeChanged));
+        #endregion
+
+        #region DesiredItemHeight
+        public double DesiredItemHeight
+        {
+            get { return (double)GetValue(DesiredItemHeightProperty); }
+            set { SetValue(DesiredItemHeightProperty, value); }
+        }
+
+        public static readonly DependencyProperty DesiredItemHeightProperty = DependencyProperty.Register("DesiredItemHeight", typeof(double), typeof(ResponsiveGridViewPanelEx), new PropertyMetadata(0.0, DesiredSizeChanged));
+        #endregion
+
+        #region OneRowModeEnabled
+        public bool OneRowModeEnabled
+        {
+            get { return (bool)GetValue(OneRowModeEnabledProperty); }
+            set { SetValue(OneRowModeEnabledProperty, value); }
+        }
+
+        public static readonly DependencyProperty OneRowModeEnabledProperty = DependencyProperty.Register("OneRowModeEnabled", typeof(bool), typeof(ResponsiveGridViewPanelEx), new PropertyMetadata(false, DesiredSizeChanged));
+        #endregion
+
+        private static void DesiredSizeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as ResponsiveGridViewPanelEx;
+            control.InvalidateMeasure();
+        }
+
+        private double InternalItemWidth
+        {
+            get
+            {
+                double itemWidth = this.DesiredItemWidth;
+                itemWidth = itemWidth > 0.0 ? itemWidth : this.DesiredItemHeight;
+                itemWidth = itemWidth > 0.0 ? itemWidth : 200.0;
+                return itemWidth;
+            }
+        }
+
+        private double InternalItemHeight
+        {
+            get
+            {
+                double itemHeight = this.DesiredItemHeight;
+                itemHeight = itemHeight > 0.0 ? itemHeight : this.InternalItemWidth;
+                return itemHeight;
+            }
+        }
+
+        public double AspectRatio
+        {
+            get { return this.InternalItemHeight / this.InternalItemWidth; }
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            if (this.IsReady && base.Children.Count > 0)
+            {
+                availableSize = NormalizeSize(availableSize);
+                var itemSize = GetItemSize(availableSize);
+
+                foreach (var item in base.Children)
+                {
+                    item.Measure(itemSize);
+                }
+
+                int columns = InferColumns(availableSize.Width);
+                int rows = (int)Math.Ceiling((double)base.Children.Count / (double)columns);
+                if (this.OneRowModeEnabled)
+                {
+                    rows = 1;
+                }
+
+                return new Size(availableSize.Width, rows * itemSize.Height);
+            }
+
+            return base.MeasureOverride(availableSize);
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            if (this.IsReady && base.Children.Count > 0)
+            {
+                finalSize = NormalizeSize(finalSize);
+
+                var itemSize = GetItemSize(finalSize);
+
+                double x = 0;
+                double y = 0;
+                double h = 0;
+                foreach (var item in base.Children)
+                {
+                    h = Math.Max(y, h);
+
+                    item.Arrange(new Rect(new Point(x, y), itemSize));
+                    x += itemSize.Width;
+
+                    double nextX = x + itemSize.Width;
+                    if (nextX > finalSize.Width + 1)
+                    {
+                        if (!this.OneRowModeEnabled)
+                        {
+                            x = 0;
+                            y += itemSize.Height;
+                        }
+                    }
+                }
+
+                return new Size(finalSize.Width, h + itemSize.Height);
+            }
+
+            return base.ArrangeOverride(finalSize);
+        }
+
+        private Size GetItemSize(Size availableSize)
+        {
+            int columns = InferColumns(availableSize.Width);
+
+            double maxItemWidth = this.InternalItemWidth * 1.25;
+            if (columns > base.Children.Count)
+            {
+                if (maxItemWidth * base.Children.Count < availableSize.Width)
+                {
+                    return CreateSize(maxItemWidth);
+                }
+                return CreateSize(availableSize.Width / (columns - 1));
+            }
+
+            double cw = availableSize.Width / columns;
+            double ch = cw * this.AspectRatio;
+
+            return new Size(cw, ch);
+        }
+
+        private int InferColumns(double availableWidth)
+        {
+            return (int)Math.Max(1, availableWidth / this.InternalItemWidth);
+        }
+
+        private Size CreateSize(double width)
+        {
+            return new Size(width, width * this.AspectRatio);
+        }
+
+        #region NormalizeSize
+        private Size NormalizeSize(Size size)
+        {
+            double width = size.Width;
+            double height = size.Height;
+
+            if (double.IsInfinity(width))
+            {
+                width = Window.Current.Bounds.Width;
+            }
+            if (double.IsInfinity(height))
+            {
+                height = Window.Current.Bounds.Height;
+            }
+
+            return new Size(width, height);
+        }
+        #endregion
+    }
+}
