79995d8fb9ceb11cf7d1d2f28f5659ab4ca4227d
Julia Piedrahita Alarcón <julia.piedrahita.alarcon@hotmail.es>
2016-05-25 18:11:10 +0200

    WAS user agent

---
+++ src/AppStudio.DataProviders/Core/AssemblyUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppStudio.DataProviders
+{
+    internal class AssemblyUtils
+    {
+        internal static string GetVersion()
+        {
+            var assembly = typeof(AssemblyUtils).GetTypeInfo().Assembly;
+            var assemblyName = new AssemblyName(assembly.FullName);
+            return $"{assemblyName.Version.Major}.{assemblyName.Version.Minor}.{assemblyName.Version.Build}";
+        }
+    }
+}
