1b66f1721f3b9f20cfd1a8838ccdbd20c9dd0cd0
Alvaro Mateos <alvaroma@live.com>
2016-06-23 13:22:47 +0200

    SlideShow Sample Page.

---
+++ samples/AppStudio.Uwp.Samples/Labs/SlideShow/Docs/SlideShowHelp.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace AppStudio.Uwp.Samples.Labs
+{
+    public sealed partial class SlideShowHelp : UserControl
+    {
+        public SlideShowHelp()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ samples/AppStudio.Uwp.Samples/Labs/SlideShow/SlideShowPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using System.Collections.ObjectModel;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Navigation;
+
+namespace AppStudio.Uwp.Samples.Labs
+{
+    [SamplePage(Category = "Labs", Name = "SlideShow", Order = 30)]
+    public sealed partial class SlideShowPage : SamplePage
+    {
+        public SlideShowPage() : base(true)
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+            commandBar.DataContext = this;
+            paneHeader.DataContext = this;
+        }
+
+        public override string Caption
+        {
+            get { return "SlideShow"; }
+        }
+
+        #region Items
+        public ObservableCollection<object> Items
+        {
+            get { return (ObservableCollection<object>)GetValue(ItemsProperty); }
+            set { SetValue(ItemsProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemsProperty = DependencyProperty.Register("Items", typeof(ObservableCollection<object>), typeof(SlideShowPage), new PropertyMetadata(null));
+        #endregion
+
+        #region ItemTemplate
+        public DataTemplate ItemTemplate
+        {
+            get { return (DataTemplate)GetValue(ItemTemplateProperty); }
+            set { SetValue(ItemTemplateProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemTemplateProperty = DependencyProperty.Register("ItemTemplate", typeof(DataTemplate), typeof(SlideShowPage), new PropertyMetadata(null));
+        #endregion
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.Items = new ObservableCollection<object>(PhotosDataSource.GetItems().Skip(4));
+            base.OnNavigatedTo(e);
+        }
+
+        protected override void OnSettings()
+        {
+            AppShell.Current.Shell.ShowRightPane(new SlideShowSettings() { DataContext = control });
+        }
+    }
+}
