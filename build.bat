cdc46604fa81f6f7ea752f08edef1744ce78a4a7
Raúl Alarcón <raul.alarcon@live.com>
2016-06-24 11:54:24 +0200

    Clean up previous build utilities

--- build.bat
+++
@@ -1,4 +0,0 @@
-call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\VsMSBuildCmd.bat"
-.nuget\nuget.exe restore waslibs.sln
-msbuild /p:Configuration=Release waslibs.sln
-git checkout .
