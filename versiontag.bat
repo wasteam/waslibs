d0f473bd3a8c84434f5d89b0009dd9c6fcbf677e
Raúl Alarcón <raul.alarcon@live.com>
2016-06-24 11:31:14 +0200

    Update user.name

--- versiontag.bat
+++ versiontag.bat
@@ -1,5 +1,6 @@
 git config user.email "%1@outlook.com"
-git config user.name "%2"
+git config user.name "%1"
 git remote set-url origin https://%1:%2@github.com/wasteam/waslibs.git
+git checkout %3
 git tag -a "%PackageVersionTag%" -m "Version built. %PackageVersionTag%"
 git push origin %PackageVersionTag%
